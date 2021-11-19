e55c220bce9f8c227ada129f6285decacb9fd8ae
madhurya <madhuryar017@gmail.com>
2021-11-19 22:18:00 +0530

    [madhurya] UC2 Added

--- EmployeeWages/UCEmp.cs
+++ EmployeeWages/UCEmp.cs
@@ -8,25 +8,32 @@ namespace EmployeeWages
     {
         //public const int isParttime = 1;
         public const int isFulltime = 1;
-        //public const int empRateHrs = 20;
+        public const int empRateHrs = 20;
         public void UCEwage()
         {
-           // int empHrs = 0;
-            //int empWage = 0;
-            Console.WriteLine("UC1");
+            int empHrs = 0;
+            int empWage = 0;
+            Console.WriteLine("UC2");
             Console.WriteLine("-------------------------------");
             Random rand = new Random();
             int empCheck = rand.Next(0, 2);
             if (empCheck == isFulltime)
             {
-                Console.WriteLine("Employee is Present");
+                //Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
-
+            //else if (empCheck == isFulltime)
+            //{
+            //    empHrs = 8;
+            //}
             else
             {
-                Console.WriteLine("Employee is Present");
+                //Console.WriteLine("Employee is Present");
+                empHrs = 0;
             }
 
+            empWage = empHrs * empRateHrs;
+            Console.WriteLine("Emp Wage is : " + empWage);
             Console.WriteLine("-------------------------------");
 
         }
