4f2ab5a7074e981f481be1b589a3169ecb294aef
madhurya <madhuryar017@gmail.com>
2021-11-19 23:08:54 +0530

    [madhurya] Adding UC5

--- EmployeeWages/UCEmp.cs
+++ EmployeeWages/UCEmp.cs
@@ -9,43 +9,52 @@ namespace EmployeeWages
         public const int isParttime = 1;
         public const int isFulltime = 2;
         public const int empRateHrs = 20;
+        public const int numWrkDays = 2;
         public void UCEwage()
         {
             int empHrs = 0;
             int empWage = 0;
-            Random rand = new Random();
-            int empCheck = rand.Next(0, 3);
-            //if (empCheck == isParttime)
-            //{
-            //    empHrs = 4;
-            //}
-            //else if (empCheck == isFulltime)
-            //{
-            //    empHrs = 8;
-            //}
-            //else
-            //{
-            //    empHrs = 0;
-            //}
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < numWrkDays; day++)
             {
-                case isParttime:
-                    empHrs = 4;
-                    break;
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 3);
+                //if (empCheck == isParttime)
+                //{
+                //    empHrs = 4;
+                //}
+                //else if (empCheck == isFulltime)
+                //{
+                //    empHrs = 8;
+                //}
+                //else
+                //{
+                //    empHrs = 0;
+                //}
+                switch (empCheck)
+                {
+                    case isParttime:
+                        empHrs = 4;
+                        break;
 
-                case isFulltime:
-                    empHrs = 8;
-                    break;
+                    case isFulltime:
+                        empHrs = 8;
+                        break;
 
-                default:
-                    empHrs = 0;
-                    break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                Console.WriteLine("UC5");
+                Console.WriteLine("-------------------------------");
+                empWage = empHrs * empRateHrs;
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("Emp Wage is : " + empWage);
 
             }
-            Console.WriteLine("UC4");
-            Console.WriteLine("-------------------------------");
-            empWage = empHrs * empRateHrs;
-            Console.WriteLine("Emp Wage is : " + empWage);
+
+            Console.WriteLine("Total Emp Wage is : " + totalEmpWage);
             Console.WriteLine("-------------------------------");
         }
     }
