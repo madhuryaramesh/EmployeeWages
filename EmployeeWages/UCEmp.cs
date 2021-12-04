f031a291263227bd3237a0aa16edb9977a039750
madhurya <madhuryar017@gmail.com>
2021-12-04 08:22:00 +0530

    [madhurya] Adding UC9

--- EmployeeWages/UCEmp.cs
+++ EmployeeWages/UCEmp.cs
@@ -1,67 +1,79 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace EmployeeWages
-//{
-//    public class UCEmp
-//    {
-//        public const int isParttime = 1;
-//        public const int isFulltime = 2;
-//        //public const int empRateHrs = 20;
-//        //public const int numWrkDays = 2;
-//        //public const int maxHrMnth = 10;
-//        public static int UCEwage(string company,int empRateHrs,int numWrkDays, int maxHrMnth)
-//        {
-//            int empHrs = 0;
-//           // int empWage = 0;
-//            int totalEmpWage = 0;
-//            int totalEmpHrs = 0;
-//            int totalWrkDays = 0;
-//            while (totalEmpHrs <= maxHrMnth && totalWrkDays < numWrkDays)
-//            {
-//                totalWrkDays++;
-//                Random rand = new Random();
-//                int empCheck = rand.Next(0, 3);
-//                //if (empCheck == isParttime)
-//                //{
-//                //    empHrs = 4;
-//                //}
-//                //else if (empCheck == isFulltime)
-//                //{
-//                //    empHrs = 8;
-//                //}
-//                //else
-//                //{
-//                //    empHrs = 0;
-//                //}
-//                switch (empCheck)
-//                {
-//                    case isParttime:
-//                        empHrs = 4;
-//                        break;
+namespace EmployeeWages
+{
+    public class EmpWageBuilderObject
+    {
+        public const int isParttime = 1;
+        public const int isFulltime = 2;
 
-//                    case isFulltime:
-//                        empHrs = 8;
-//                        break;
+        private string company;
+        private int empRateHrs;
+        private int numWrkDays;
+        private int maxHrMnth;
+        private int totalEmpWage;
 
-//                    default:
-//                        empHrs = 0;
-//                        break;
+        //public const int empRateHrs = 20;
+        //public const int numWrkDays = 2;
+        //public const int maxHrMnth = 10;
+        public EmpWageBuilderObject(string company, int empRateHrs, int numWrkDays, int maxHrMnth)
+        {
 
-//                }
-//                Console.WriteLine("UC8");
-//                Console.WriteLine("-------------------------------");
-//                totalEmpHrs = totalEmpHrs + empHrs;
-//                Console.WriteLine("Days :" + totalWrkDays + "Employment Hours : " + empHrs);
+            this.company = company;
+            this.empRateHrs = empRateHrs;
+            this.numWrkDays = numWrkDays;
+            this.maxHrMnth = maxHrMnth;
 
-//                // Console.WriteLine("Emp Wage is : " + totalEmpWage);
+        }
 
-//            }
-//            totalEmpWage = totalEmpHrs + empRateHrs;
-//            Console.WriteLine("Total Emp Wagefor company "+company+ " is : " + totalEmpWage);
-//            return totalEmpWage;
-          
-//        }
-//    }
-//}
+        public void computeEmpWage()
+        {
+
+            int empHrs = 0;
+            // int empWage = 0;
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWrkDays = 0;
+            while (totalEmpHrs <= maxHrMnth && totalWrkDays < numWrkDays)
+            {
+                totalWrkDays++;
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 3);
+                
+                switch (empCheck)
+                {
+                    case isParttime:
+                        empHrs = 4;
+                        break;
+
+                    case isFulltime:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                Console.WriteLine("UC9");
+                Console.WriteLine("-------------------------------");
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Days :" + totalWrkDays + "Employment Hours : " + empHrs);
+
+                // Console.WriteLine("Emp Wage is : " + totalEmpWage);
+
+            }
+            totalEmpWage = totalEmpHrs + empRateHrs;
+            Console.WriteLine("Total Emp Wagefor company " + company + " is : " + totalEmpWage);
+           
+        
+        }
+        public string toString()
+        {
+            return "Total Emp Wagefor company " + this.company + " is : " + this.totalEmpWage;
+;        }
+    }
+    
+}
